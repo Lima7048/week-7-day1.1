6c80525ea66eccc3335af7811e1b6aa6cbbffe28
salima <brycesalima@gmail.com>
2024-02-19 14:43:51 +0000

    multiple forms

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-import { Form } from "./component/Form";
+import Form from "./component/Form";
 
 export default function App() {
-  // return <Form />;
+  return <Form />;
 }
